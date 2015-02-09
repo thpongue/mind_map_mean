c61d90adc4a988105163be6bc813b3d538ccf4e6
Dominic McAdden <dominic.mcadden@gmail.com>
2015-02-09 17:18:48 +0000

    enabling browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,9 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  })
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		browserify: {
+			'build/app.js': ['js/app.js']
+		}
+	})
+	grunt.loadNpmTasks('grunt-browserify')
 }
