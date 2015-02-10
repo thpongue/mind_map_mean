6f045316706eb6caf8074a2b8308820cfacbed17
Dominic McAdden <dominic.mcadden@gmail.com>
2015-02-10 15:35:33 +0000

    since minification messes up Angular I've added an ng-annotate step into the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,19 @@
-module.exports = function(grunt) {
+odule.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		ngAnnotate: {
+			demo: {
+				files: {
+					'js/main.js.annotated':['js/main.js']	
+				}
+			}
+		},
 		browserify: {
-			'build/app.js': ['js/app.js']
+			'build/app.js': ['js/main.js.annotated']
 		}
 	})
+	grunt.loadNpmTasks('grunt-ng-annotate');
 	grunt.loadNpmTasks('grunt-browserify')
+
+	grunt.registerTask('default', ['ngAnnotate', 'browserify']);
 }
